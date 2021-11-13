a767074342214548dad6ef704a85efb93e1e4ec2
Tan Le <19211tt1603@mail.tdc.edu.vn>
2021-11-13 17:05:29 +0700

    Categories

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\FuncCall;
 use App\Models\Product;
+use App\Models\Category;
 
 class PageController extends Controller
 {
@@ -12,7 +13,9 @@ class PageController extends Controller
     public function index()
     {
         $products = Product::orderBy('id', 'DESC')->get();
-        return view('index')->with('products', $products);
+        $categories = Category::orderBy('id', 'DESC')->get();
+        return view('index')->with('products', $products)
+        ->with('categories', $categories);
     }
 
     public function getSearch(Request $req)
@@ -22,5 +25,14 @@ class PageController extends Controller
         ->get();
 
         return view('page.search', compact('product'));
+    }
+
+    public function category($cat_id)
+    {
+        $categories = Category::orderBy('name', 'ASC')->get();
+        $cate = Category::where('id', $cat_id)->first();
+        $product = Product::where('cat_id',$cate->id)->get();
+        return view('page.category')->with('categories', $cate)
+        ->with('products', $product);
     }
 }

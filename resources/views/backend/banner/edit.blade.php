d9586df37e91cc585d95c2d4962fea5f00191192
nhquyen <nguyenhuuquyen2001@gmail.com>
2021-11-16 09:14:10 +0700

    Management banner

---
+++ resources/views/backend/banner/edit.blade.php
@@ -0,0 +1,91 @@
+@extends('backend.layouts.master')
+@section('title','E-SHOP || Banner Edit')
+@section('main-content')
+
+<div class="card">
+    <h5 class="card-header">Edit Banner</h5>
+    <div class="card-body">
+        <form method="post" action="{{route('banner.update',$banner->id)}}">
+            @csrf
+            @method('PATCH')
+            <div class="form-group">
+                <label for="inputTitle" class="col-form-label">Title <span class="text-danger">*</span></label>
+                <input id="inputTitle" type="text" name="title" placeholder="Enter title" value="{{$banner->title}}"
+                    class="form-control">
+                @error('title')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="inputDesc" class="col-form-label">Description</label>
+                <textarea class="form-control" id="description" name="description">{{$banner->description}}</textarea>
+                @error('description')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="inputDesc" class="col-form-label">Button link</label>
+                <textarea class="form-control" id="link" name="link">{{$banner->link}}</textarea>
+                @error('link')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="inputPhoto" class="col-form-label">Photo <span class="text-danger">*</span></label>
+                <div class="input-group">
+                    <span class="input-group-btn">
+                        <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                            <i class="fa fa-picture-o"></i> Choose
+                        </a>
+                    </span>
+                    <input id="thumbnail" class="form-control" type="text" name="image" value="{{$banner->image}}">
+                </div>
+                <div id="holder" style="margin-top:15px;max-height:100px;"></div>
+                @error('image')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="status" class="col-form-label">Status <span class="text-danger">*</span></label>
+                <select name="status" class="form-control">
+                    @if($countActive == 5)
+                    @if($banner->status == 'active')
+                    <option value="active" {{(($banner->status=='active') ? 'selected' : '')}}>Active</option>
+                    @endif
+                    <option value="inactive" {{(($banner->status=='inactive') ? 'selected' : '')}}>Inactive</option>
+                    @else
+                    <option value="inactive" {{(($banner->status=='inactive') ? 'selected' : '')}}>Inactive</option>
+                    <option value="active" {{(($banner->status=='active') ? 'selected' : '')}}>Active</option>
+                    @endif
+                </select>
+                @error('status')
+                <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <div class="form-group mb-3">
+                <button class="btn btn-success" type="submit">Update</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
+
+@push('styles')
+<link rel="stylesheet" href="{{asset('backend/summernote/summernote.min.css')}}">
+@endpush
+@push('styles')
+<link rel="stylesheet" href="{{asset('backend/summernote/summernote.min.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
+@endpush
+@push('scripts')
+<script>
+var route_prefix = "{{url('/filemanager')}}";
+$('#lfm').filemanager('image', {
+    prefix: route_prefix
+});
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
+@endpush

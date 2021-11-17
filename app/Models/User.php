9c06e20fcd595ced32c72ed9bbc5dca4134ff1f0
Longkunz <Cuvip1325@gmail.com>
2021-11-17 19:47:09 +0700

    Fix link

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    //Đếm số category đang active
+    public static function countActiveUser()
+    {
+        $data = User::where('status', 'active')->count();
+        if ($data) {
+            return $data;
+        }
+        return 0;
+    }
 }

fd9b4bfa722ae445b4b50fb19f2bd06d41166671
Longkunz <Cuvip1325@gmail.com>
2021-11-17 12:49:18 +0700

    Show index page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Banner;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\FuncCall;
 use App\Models\Product;
@@ -14,15 +15,27 @@ class PageController extends Controller
     //Index page
     public function index()
     {
-        $products = Product::orderBy('id', 'DESC')->get();
-        return view('index')->with('products', $products);
+        $products = Product::orderBy('id', 'DESC')->get()->take(16);
+        $banners = Banner::orderBy('id', 'DESC')->where('status', 'active')->get();
+        $cate1 = Category::where('status', 'active')->where('id', 1)->first();
+        $cate2 = Category::where('status', 'active')->where('id', 2)->first();
+        $cate3 = Category::where('status', 'active')->where('id', 3)->first();
+        $cate4 = Category::where('status', 'active')->where('id', 4)->first();
+        $best = Cart::with('product')->get()->take(8);
+        return view('index')->with('products', $products)
+            ->with('banners', $banners)
+            ->with('cate1', $cate1)
+            ->with('cate2', $cate2)
+            ->with('cate3', $cate3)
+            ->with('cate4', $cate4)
+            ->with('best', $best);
     }
 
     public function getSearch(Request $req)
     {
-        $product = Product::where('title', 'like','%'.$req->key.'%')
-        ->orwhere ('summary', $req->key)
-        ->get();
+        $product = Product::where('title', 'like', '%' . $req->key . '%')
+            ->orwhere('summary', $req->key)
+            ->get();
 
         return view('page.search', compact('product'));
     }

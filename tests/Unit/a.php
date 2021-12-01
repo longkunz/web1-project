7ff619c3175333dae22d9dd85c359c13b421ed95
Longkunz <Cuvip1325@gmail.com>
2021-12-01 15:49:37 +0700

    Update setting unit test

--- tests/Unit/a.php
+++ tests/Unit/a.php
@@ -1,29 +1,25 @@
 <?php
-public function test_placeOrder()
+//Test update setting valid input
+public function test_settingValid()
 {
-    $order = new Order();
-    $order_data['order_number'] = "ORD-123456";
-    $order_data['user_id'] = "1";
-    $order_data['sub_total'] = "1000";
-    $order_data['quantity'] = "5";
-    $order_data['shipping'] = "10";
-    $order_data['payment_status'] = "unpaid";
-    $order_data['total_amount'] = "5005";
-    $order_data['address'] = "53 vo van ngan";
-    $order_data['name'] = "Ho Viet Long";
-    $order_data['email'] = "hovietlong234@gmail.com";
-    $order_data['phone'] = "0382987829";
-    $order_data['status'] = "new";
+    $setting = new Setting();
+    $data['description'] = "Description";
+    $data['title'] = "Title";
+    $data['logo'] = "logo";
+    $data['address'] = "address";
+    $data['phone'] = "phone";
+    $data['email'] = "email";
     DB::beginTransaction();
-    $order->fill($order_data);
-    $order->save();
-    $actual = $order->where("phone", "0382987829")->first();
-    $this->assertEquals("0382987829", $actual->phone);
+    $stored = $setting->where("id", "1")->first();
+    $status = $stored->fill($data)->save();
+    $actual = $setting->where("id", "1")->first();
+    $this->assertEquals("Title", $actual->title);
     DB::rollBack();
 
+
     try {
-        $order->fill($order_data);
-        $order->save();
+        $stored = $setting->where("id", "1")->first();
+    $status = $stored->fill($data)->save();
     } catch (Throwable $e) {
         $this->assertTrue(true);
     }
